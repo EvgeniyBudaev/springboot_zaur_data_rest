6a9ac828a628d386d84ccb5dd648576d555fe3e7
Evgeniy Budaev <budaev.e@gmail.com>
2022-11-10 21:17:02 +0300

    Spring Boot Data Rest

---
+++ src/main/java/com/example/springboot_zaur_data_rest/SpringbootZaurDataRestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot_zaur_data_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootZaurDataRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootZaurDataRestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springboot_zaur_data_rest/SpringbootZaurDataRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboot_zaur_data_rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootZaurDataRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
